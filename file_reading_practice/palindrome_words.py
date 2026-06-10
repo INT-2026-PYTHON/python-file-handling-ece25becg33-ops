95f1bd1fd736b06bfe6168a6f4f27faf6dfa3f56
Swetapadma Parida <ece.25becg33@silicon.ac.in>
2026-06-10 18:26:04 +0530

    Implement palindrome detection and counting
    Added functions to check for palindromes and count them in a file.

--- file_reading_practice/palindrome_words.py
+++ file_reading_practice/palindrome_words.py
@@ -51,3 +51,19 @@ Explanation:
 =================================================
 
 """
+def is_palindrome(word):
+    return word == word[::-1]
+
+def find_palindromes(filename):
+    count = 0
+
+    with open(filename, "r") as file:
+        for word in file:
+            word = word.strip().lower()
+            if is_palindrome(word):
+                print(word)
+                count += 1
+
+    print("Total palindromes:", count)
+
+find_palindromes("sowpods.txt")
